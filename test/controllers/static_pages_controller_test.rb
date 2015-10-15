2b99929b7b0d852d7877cd78979a2ed06027bde3
Héctor Morales <HectorMg@Hector-Mg.local>
2015-10-14 22:13:16 -0500

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Free Enterprise Leadership Challenge"
   end
 
   test "should get company" do
     get :company
     assert_response :success
+    assert_select "title", "Companies | Free Enterprise Leadership Challenge"
   end
 
   test "should get schedule" do
     get :schedule
     assert_response :success
+    assert_select "title", "Schedule | Free Enterprise Leadership Challenge"
   end
 
   test "should get manual" do
     get :manual
     assert_response :success
+    assert_select "title", "Manual | Free Enterprise Leadership Challenge"
   end
 
 end
