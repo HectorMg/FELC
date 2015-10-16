695095c29403df3dd6551aab938ec0fecd4271a9
Héctor Morales <HectorMg@Hector-Mg.local>
2015-10-15 20:17:28 -0500

    Finsih layout and routes for existing views

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,11 +3,22 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
+
+    get root_path
+    assert_template 'landing_page/landing'
+    assert_select "a[href=?]", home_path, count: 2
+    assert_select "a[href=?]", signup_path
+
     get home_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", home_path
+    assert_select "a[href=?]", home_path, count: 2
     assert_select "a[href=?]", company_path
     assert_select "a[href=?]", schedule_path
     assert_select "a[href=?]", manual_path
+
+    get signup_path
+    assert_template 'users/new'
+    assert_select "a[href=?]", home_path
+    assert_select "a[href=?]", signup_path
   end
 end
