289676d059fdeaf219bc0c1ca2ea1517e6a745d5
Héctor Morales <HectorMg@Hector-Mg.local>
2015-10-24 23:39:47 -0500

    Refactor User model to be more idiomatically correct and refactor integration test suite and user controller to match

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,15 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length:{ minimum: 6 }
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
 
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
+    def self.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def self.new_token
+      SecureRandom.urlsafe_base64
+    end
 
   def remember
     self.remember_token = User.new_token
