43b44186ea92168670233c1e1d366e7848772f9b
HectorMg <hectormgwake@gmail.com>
2016-01-05 15:41:13 -0600

    Adds bank_system stylesheet to the asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.precompile += %w( landing_page.css )
 Rails.application.config.assets.precompile += %w( users.css )
 Rails.application.config.assets.precompile += %w( dashboard.css )
 Rails.application.config.assets.precompile += %w( companies.css )
+Rails.application.config.assets.precompile += %w( bank_system.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
