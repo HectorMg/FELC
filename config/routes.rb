96d751671611017cc5d3cb0e982f1602479b8328
HectorMg <hectormgwake@gmail.com>
2016-01-06 11:02:41 -0600

    Fixes routing issues between Static pages and company model resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   root                  'landing_page#landing'
   get 'home'         => 'static_pages#home'
-  get 'company'      => 'static_pages#company'
+  get 'teams'      => 'static_pages#company'
   get 'schedule'     => 'static_pages#schedule'
   get 'manual'       => 'static_pages#manual'
   get 'signup'       => 'users#new'
