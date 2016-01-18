670ed5cf2c0bef8bc678884725a9215e39283251
HectorMg <hectormgwake@gmail.com>
2016-01-18 14:13:11 -0600

    Eliminates referencing migration

--- app/models/company_account.rb
+++ app/models/company_account.rb
@@ -1,6 +1,5 @@
 class CompanyAccount < ActiveRecord::Base
   has_many :users
   has_many :staffs
-  has_many :sent_transactions, :class_name => 'Transaction', :foreign_key => 'provider_id'
-  has_many :received_transactions, :class_name => 'Transaction', :foreign_key => 'customer_id'
+  has_many :transactions
 end
--- db/migrate/20160118195432_add_references_to_transactions.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToTransactions < ActiveRecord::Migration
-  def change
-    add_reference :transactions, :provider, index: true, foreign_key: true
-    add_reference :transactions, :customer, index: true, foreign_key: true
-  end
-end
