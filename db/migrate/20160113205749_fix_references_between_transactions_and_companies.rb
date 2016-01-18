244967f14125be25feae01f1142675a2c16b7f0e
HectorMg <hectormgwake@gmail.com>
2016-01-18 13:29:18 -0600

    Remove migration to fix issues

--- db/migrate/20160113205749_fix_references_between_transactions_and_companies.rb
+++
@@ -1,6 +0,0 @@
-class FixReferencesBetweenTransactionsAndCompanies < ActiveRecord::Migration
-  def change
-    remove_column :transactions, :provider_id
-    remove_column :transactions, :customer_id
-  end
-end
