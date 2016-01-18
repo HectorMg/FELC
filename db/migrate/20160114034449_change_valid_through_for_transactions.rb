9d5da2cb6e8dc17f8f574537ead5b9951bc65bd8
HectorMg <hectormgwake@gmail.com>
2016-01-17 22:08:28 -0600

    Removes unnecessary migration that removed start and end date from transactions

--- db/migrate/20160114034449_change_valid_through_for_transactions.rb
+++
@@ -1,7 +0,0 @@
-class ChangeValidThroughForTransactions < ActiveRecord::Migration
-  def change
-    remove_column :transactions, :start_date
-    remove_column :transactions, :end_date
-    add_column :transactions, :valid_through, :text
-  end
-end
