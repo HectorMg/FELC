6d4d0f18b42cf7169a7753e2ca1a57790f218277
HectorMg <hectormgwake@gmail.com>
2016-01-18 18:55:57 -0600

    Adds Transactions table and seeds for companies

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :provider, :class_name => 'CompanyAccount'
-  belongs_to :customer, :class_name => 'CompanyAccount'
+  belongs_to :providers, :class_name => 'CompanyAccount'
+  belongs_to :customers, :class_name => 'CompanyAccount'
 
   validates :description, presence: true
   validates :amount, presence: true
